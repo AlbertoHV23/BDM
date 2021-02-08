315f775b1342a93a029754e0b9bde81a7b327540
Alberto Hernandez <albertho0.hdzv@gmail.com>
2021-02-07 18:06:47 -0600

    MAIN PAGE

---
+++ aseets/js/main.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  var barras = document.querySelector("#barras");
+  var times = document.querySelector("#times");
+  header.classList.toggle("sticky", window.scrollY > 10);
+  barras.classList.toggle("black", window.scrollY > 10);
+  times.classList.toggle("black", window.scrollY > 10);
+});
+
+
+
+
+function abrir() {
+  var menutoggle = document.querySelector(".menu");
+  if (screen.width < 991) {
+    document.getElementById("barras").style.display = "none";
+    document.getElementById("times").style.display = "block";
+    menutoggle.classList.toggle("active");
+  }
+}
+
+function cerrar() {
+  if (screen.width < 991) {
+    var menutoggle = document.querySelector(".menu");
+    document.getElementById("barras").style.display = "block";
+    document.getElementById("times").style.display = "none";
+    menutoggle.classList.toggle("active");
+  }
+}
