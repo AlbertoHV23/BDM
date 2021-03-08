1b375b3957760f0bb679fc3e8db5dca2a6d256cf
Alberto Hernandez <albertho0.hdzv@gmail.com>
2021-03-08 00:17:48 -0600

    Nueva seccion para crear curso - pero no pude

--- Dash.js
+++ Dash.js
@@ -2,10 +2,20 @@ $(document).ready(function(){
     $("#btn_dashboard").click(function(){
         $("#DASHBOARD").show();
         $("#MyCourse").hide();
+        $("#CreateCourse").hide();
+
     });
 
     $("#btn_mycourse").click(function(){
         $("#MyCourse").show();
+        $("#DASHBOARD").hide();
+        $("#CreateCourse").hide();
+
+    });
+
+    $("#btn_create").click(function(){
+        $("#CreateCourse").show();
+        $("#MyCourse").hide();
         $("#DASHBOARD").hide();
     });
 
