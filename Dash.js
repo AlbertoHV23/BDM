c1b09eb4bbfd279d5f8b5579190f03097e780075
Alberto Hernandez <albertho0.hdzv@gmail.com>
2021-03-11 01:05:12 -0600

    PAGO TEMPORAL

--- Dash.js
+++ Dash.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         $("#MyCourse").hide();
         $("#CreateCourse").hide();
         $("#Statics").hide();
+        $("#TecaherProfile").hide();
+
 
 
     });
@@ -13,6 +15,8 @@ $(document).ready(function(){
         $("#DASHBOARD").hide();
         $("#CreateCourse").hide();
         $("#Statics").hide();
+        $("#TecaherProfile").hide();
+
 
 
     });
@@ -22,11 +26,23 @@ $(document).ready(function(){
         $("#MyCourse").hide();
         $("#DASHBOARD").hide();
         $("#Statics").hide();
+        $("#TecaherProfile").hide();
+
 
     });
 
     $("#btn_statics").click(function(){
         $("#Statics").show();
+        $("#CreateCourse").hide();
+        $("#MyCourse").hide();
+        $("#DASHBOARD").hide();
+        $("#TecaherProfile").hide();
+
+    });
+
+    $("#btn_profile").click(function(){
+        $("#TecaherProfile").show();
+        $("#Statics").hide();
         $("#CreateCourse").hide();
         $("#MyCourse").hide();
         $("#DASHBOARD").hide();
