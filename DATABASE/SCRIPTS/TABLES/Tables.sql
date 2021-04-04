1f905eafebf7f53a42e21e155757d6b6fd22f8a5
gerardobecerra1 <42364794+gerardobecerra1@users.noreply.github.com>
2021-04-03 19:29:01 -0600

    update

--- DATABASE/SCRIPTS/TABLES/Tables.sql
+++ DATABASE/SCRIPTS/TABLES/Tables.sql
@@ -1,17 +1,14 @@
 CREATE DATABASE IF NOT EXISTS changetheworld;
-
 USE changetheworld;
 
 CREATE TABLE IF NOT EXISTS tbl_Categories (
-  id_categorie 			INT UNSIGNED 		AUTO_INCREMENT 					COMMENT'This field represents the identifier of the category.',
-  name_categorie 		VARCHAR(255) 		NOT NULL						COMMENT'This field represents the name of the category.',
-  
-  PRIMARY KEY(id_categorie) 												COMMENT'This field represents pk of the table categories'
-);
+  id_categorie		INT UNSIGNED    AUTO_INCREMENT		COMMENT'This field represents the identifier of the category.',
+  name_categorie 	VARCHAR(255)    NOT NULL			COMMENT'This field represents the name of the category.',
+  PRIMARY KEY(id_categorie))  COMMENT'This table represents all the categories of the website.';
 
 CREATE TABLE IF NOT EXISTS tbl_Users (
-  id_user 				INT UNSIGNED 		AUTO_INCREMENT 		COMMENT'This field represents the user identifier.',
-  role_user 			VARCHAR(10) 		NOT NULL 			COMMENT'This field represents the user role.',
+  id_user				INT UNSIGNED		AUTO_INCREMENT		COMMENT'This field represents the user identifier.',
+  role_user 			VARCHAR(10)			NOT NULL 			COMMENT'This field represents the user role.',
   photo 				BLOB 				NULL 				COMMENT'This field represents the user photo.',
   username 				VARCHAR(255) 		NOT NULL 			COMMENT'This field represents the user name.',
   name_user 			VARCHAR(255) 		NOT NULL 			COMMENT'This field represents the name.',
@@ -21,19 +18,16 @@ CREATE TABLE IF NOT EXISTS tbl_Users (
   password_user 		VARCHAR(255) 		NOT NULL 			COMMENT'This field represents the password of the user account.',
   registered_date 		DATE 				NOT NULL 			COMMENT'This field represents the day the user registered.',
   date_of_last_change 	DATE 				NOT NULL 			COMMENT'This field represents the last change that was made to the users data.',
-  
-PRIMARY KEY(id_user) 											COMMENT'This field represents pk of the table users'
-);
+  PRIMARY KEY(id_user))	COMMENT'This table represents the data of all registered users on the website.';
 
 CREATE TABLE IF NOT EXISTS tbl_Schools(
  id_school 					INT UNSIGNED 		 AUTO_INCREMENT 		COMMENT'This field represents the identifier of the created school.',
  fk_user 					INT UNSIGNED 		 NOT NULL 				COMMENT'This field represents who created the school.',
  logo 						BLOB 				 NOT NULL 				COMMENT'This field represents the school logo.',
  name_school 				VARCHAR(255) 		 NOT NULL UNIQUE 		COMMENT'This field represents the name of the school.',
- #number_of_courses 			INT 				 NULL 					COMMENT'This field represents how many courses the school has.',
  
-PRIMARY KEY(id_school)													COMMENT'This field represents pk of the table categories',
-FOREIGN KEY(fk_user) REFERENCES tbl_Users(id_user))						COMMENT'This field represents fk of the table schools';
+PRIMARY KEY(id_school),
+FOREIGN KEY(fk_user) REFERENCES tbl_Users(id_user))	COMMENT'This table represents the data of all the schools created by the registered teacher users on the website.';
 
 CREATE TABLE IF NOT EXISTS tbl_Courses(
 id_course 				INT UNSIGNED 		AUTO_INCREMENT 		COMMENT'This field represents the identifier of the course.',
@@ -50,8 +44,7 @@ cost 					DECIMAL 			NOT NULL 			COMMENT'This field represents how much the cour
 PRIMARY KEY(id_course),
 FOREIGN KEY(fk_school) 		REFERENCES tbl_Schools(id_school),			
 FOREIGN KEY(fk_categorie) 	REFERENCES tbl_Categories(id_categorie),		
-FOREIGN KEY(fk_user) 		REFERENCES tbl_Users(id_user)
-) ;
+FOREIGN KEY(fk_user) 		REFERENCES tbl_Users(id_user))	COMMENT'This table represents the data of all the courses created by the master users registered on the website.';
 
 CREATE TABLE IF NOT EXISTS tbl_Videos(
 id_video 			INT UNSIGNED  		AUTO_INCREMENT 		COMMENT'This field represents the identifier of the video.',
@@ -62,94 +55,87 @@ content 			LONGBLOB 			NOT NULL 			COMMENT'This field represents the video.',
 viewed 				BOOLEAN 			NOT NULL DEFAULT 0 	COMMENT'This field represents whether the video has been viewed or not.',
 level_				int					NOT NULL			COMMENT'This field represents  level of the coruse (basic,expert, etc).',
 PRIMARY KEY(id_video),
-FOREIGN KEY(fk_course) REFERENCES tbl_Courses(id_course))COMMENT'This table represents all the videos of the courses on the website.';
+FOREIGN KEY(fk_course) REFERENCES tbl_Courses(id_course))	COMMENT'This table represents all the videos of the courses on the website.';
 
 CREATE TABLE IF NOT EXISTS tbl_Resources(
-id_resource 			INT UNSIGNED 			AUTO_INCREMENT 			COMMENT'This field represents the identifier of the video notes.',
-fk_video 			INT UNSIGNED 				NOT NULL 				COMMENT'This field represents which video the note is from.',
-fk_user 			INT UNSIGNED 				NULL 					COMMENT'This field represents by whom the note was written.',
-note 				TEXT	 					NULL 					COMMENT'This field represents the content of the note.',
-categorie			VARCHAR(20)					NOT NULL				COMMENT'This field represents the categorie of the reource (files,media,link).',
+id_resource		INT UNSIGNED		AUTO_INCREMENT		COMMENT'This field represents the identifier of the video notes.',
+fk_video		INT UNSIGNED 		NOT NULL 			COMMENT'This field represents which video the note is from.',
+fk_user 		INT UNSIGNED 	  	NULL 				COMMENT'This field represents by whom the note was written.',
+note 			TEXT	 			NULL 				COMMENT'This field represents the content of the note.',
+categorie		VARCHAR(20)			NOT NULL			COMMENT'This field represents the categorie of the reource (files,media,link).',
 PRIMARY KEY(id_resource),
 FOREIGN KEY(fk_video) REFERENCES tbl_Videos(id_video),
-FOREIGN KEY(fk_user) REFERENCES tbl_Users(id_user)) 		COMMENT'This table represents all the notes made by users in the video.';
+FOREIGN KEY(fk_user) REFERENCES tbl_Users(id_user)) COMMENT'This table represents all the notes made by users in the video.';
 
 CREATE TABLE IF NOT EXISTS tbl_Comments_Courses(
-id_comment_course 			INT UNSIGNED 			AUTO_INCREMENT 		COMMENT'This field represents the identifier of the course comment.',
-fk_course 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents the course comments',
-fk_sender 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents who made the comment in the course.',
-message 					TEXT 					NOT NULL 			COMMENT'This field represents the content of the comment in the course.',
-comment_date				DATE 					NOT NULL			COMMENT'This field represents the date of comments',				
+id_comment_course		INT UNSIGNED		AUTO_INCREMENT		COMMENT'This field represents the identifier of the course comment.',
+fk_course 				INT UNSIGNED 		NOT NULL 			COMMENT'This field represents the course comments',
+fk_sender 				INT UNSIGNED 		NOT NULL 			COMMENT'This field represents who made the comment in the course.',
+message 				TEXT 				NOT NULL 			COMMENT'This field represents the content of the comment in the course.',
+comment_date			DATE 				NOT NULL			COMMENT'This field represents the date of comments',				
 PRIMARY KEY(id_comment_course),
 FOREIGN KEY(fk_course) REFERENCES tbl_Courses(id_course),
-FOREIGN KEY(fk_sender) REFERENCES tbl_Users(id_user)
-);
+FOREIGN KEY(fk_sender) REFERENCES tbl_Users(id_user)) COMMENT'This table represents the feedback data for the course.';
 
 CREATE TABLE IF NOT EXISTS tbl_Comments_videos(
-id_comment_video			INT UNSIGNED 			AUTO_INCREMENT 		COMMENT'This field represents the identifier of the video comment.',
-fk_video 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents the video comments',
-fk_sender 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents who made the comment in the course.',
-message 					TEXT 					NOT NULL 			COMMENT'This field represents the content of the comment in the course.',
-comment_date				DATE 					NOT NULL			COMMENT'This field represents the date of comments',				
+id_comment_video		INT UNSIGNED	AUTO_INCREMENT		COMMENT'This field represents the identifier of the video comment.',
+fk_video 				INT UNSIGNED 	NOT NULL 			COMMENT'This field represents the video comments',
+fk_sender 				INT UNSIGNED 	NOT NULL 			COMMENT'This field represents who made the comment in the course.',
+message 				TEXT 			NOT NULL 			COMMENT'This field represents the content of the comment in the course.',
+comment_date			DATE 			NOT NULL			COMMENT'This field represents the date of comments',				
 PRIMARY KEY(id_comment_video),
 FOREIGN KEY(fk_video) REFERENCES tbl_Videos(id_video),
-FOREIGN KEY(fk_sender) REFERENCES tbl_Users(id_user)
-);
-
-
+FOREIGN KEY(fk_sender) REFERENCES tbl_Users(id_user)) COMMENT'This table represents the data for the video comments.';
 
 CREATE TABLE IF NOT EXISTS Comments_comments_course (
-id_comment_coments_course	INT UNSIGNED 			AUTO_INCREMENT 		COMMENT'This field represents the identifier of the course comment.',
-fk_comment 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents the parent comments.',
-fk_sender 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents who made the comment in the comments.',
-message 					TEXT 					NOT NULL 			COMMENT'This field represents the content of the comment .',
-comment_date				DATE 					NOT NULL			COMMENT'This field represents the date of comments',				
+id_comment_coments_course		INT UNSIGNED 			AUTO_INCREMENT		COMMENT'This field represents the identifier of the course comment.',
+fk_comment 						INT UNSIGNED 			NOT NULL 			COMMENT'This field represents the parent comments.',
+fk_sender 					 	INT UNSIGNED 			NOT NULL 			COMMENT'This field represents who made the comment in the comments.',
+message 					    TEXT 					NOT NULL 			COMMENT'This field represents the content of the comment .',
+comment_date				    DATE 					NOT NULL			COMMENT'This field represents the date of comments',				
 PRIMARY KEY(id_comment_coments_course),
-FOREIGN KEY(fk_comment) REFERENCES tbl_Comments_Courses(id_comment_course)
-);
+FOREIGN KEY(fk_comment) REFERENCES tbl_Comments_Courses(id_comment_course)) COMMENT'This table represents the data from comments to comments for the course.';
 
 CREATE TABLE IF NOT EXISTS Comments_comments_video (
-id_comment_coments_video	INT UNSIGNED 			AUTO_INCREMENT 		COMMENT'This field represents the identifier of the course comment.',
-fk_comment 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents the parent comments.',
-fk_sender 					INT UNSIGNED 			NOT NULL 			COMMENT'This field represents who made the comment in the comments.',
-message 					TEXT 					NOT NULL 			COMMENT'This field represents the content of the comment .',
-comment_date				DATE 					NOT NULL			COMMENT'This field represents the date of comments',				
+id_comment_coments_video		INT UNSIGNED		AUTO_INCREMENT		COMMENT'This field represents the identifier of the course comment.',
+fk_comment 					    INT UNSIGNED 		NOT NULL 			COMMENT'This field represents the parent comments.',
+fk_sender 					    INT UNSIGNED 		NOT NULL 			COMMENT'This field represents who made the comment in the comments.',
+message 					    TEXT 				NOT NULL 			COMMENT'This field represents the content of the comment .',
+comment_date				    DATE 				NOT NULL			COMMENT'This field represents the date of comments',				
 PRIMARY KEY(id_comment_coments_video),
-FOREIGN KEY(fk_comment) REFERENCES tbl_Comments_videos(id_comment_video)
-);
-
+FOREIGN KEY(fk_comment) REFERENCES tbl_Comments_videos(id_comment_video)) COMMENT'This table represents the data from comments to video comments.';
 
 CREATE TABLE IF NOT EXISTS Messages(
-id_message 			INT UNSIGNED 			AUTO_INCREMENT 				COMMENT'This field represents the identifier of the message.',
-fk_sender 			INT UNSIGNED			NOT NULL					COMMENT'This field represents who sent the message.',
-fk_addressee 		INT UNSIGNED			NOT NULL					COMMENT'This field represents who receives the message.',
-message 			TEXT 					NOT NULL					COMMENT'This field represents the content of the message.',
-date_message 		DATETIME 				NOT NULL					COMMENT'This field represents the day the message was sent.',
+id_message		INT UNSIGNED		AUTO_INCREMENT		COMMENT'This field represents the identifier of the message.',
+fk_sender 		INT UNSIGNED		NOT NULL			COMMENT'This field represents who sent the message.',
+fk_addressee 	INT UNSIGNED		NOT NULL			COMMENT'This field represents who receives the message.',
+message 		TEXT 				NOT NULL			COMMENT'This field represents the content of the message.',
+date_message 	DATETIME 			NOT NULL			COMMENT'This field represents the day the message was sent.',
 PRIMARY KEY(id_message),
 FOREIGN KEY(fk_sender) REFERENCES tbl_Users(id_user),
-FOREIGN KEY(fk_addressee) REFERENCES tbl_Users(id_user)) 				COMMENT'This table represents the messages made by users in private';
+FOREIGN KEY(fk_addressee) REFERENCES tbl_Users(id_user))  COMMENT'This table represents the messages made by users in private';
 
 CREATE TABLE IF NOT EXISTS Ratings(
-id_rating 			INT UNSIGNED  			AUTO_INCREMENT 			COMMENT'This field represents the grade identifier in the course.',
-fk_user 			INT UNSIGNED 			NOT NULL 				COMMENT'This field represents who graded the course.',
-fk_course 			INT UNSIGNED 			NOT NULL 				COMMENT'This field represents which course was graded.',
-points 				DECIMAL(1,1) 			NOT NULL 				COMMENT'This field represents the rating that the user gave.',
+id_rating		INT UNSIGNED		AUTO_INCREMENT		COMMENT'This field represents the grade identifier in the course.',
+fk_user 		INT UNSIGNED 		NOT NULL 			COMMENT'This field represents who graded the course.',
+fk_course 		INT UNSIGNED 		NOT NULL 			COMMENT'This field represents which course was graded.',
+points			DECIMAL(1,1) 		NOT NULL 			COMMENT'This field represents the rating that the user gave.',
 PRIMARY KEY(id_rating),
 FOREIGN KEY(fk_user) REFERENCES tbl_Users(id_user),
 FOREIGN KEY(fk_course) REFERENCES tbl_Courses(id_course)) COMMENT'This table represents the grade of each user to the course.';
 
 CREATE TABLE IF NOT EXISTS Sales(
-id_sale 			INT 					AUTO_INCREMENT 			COMMENT'This field represents the identifier of the sale.',
-fk_seller 			INT UNSIGNED 			NOT NULL 				COMMENT'This field represents the seller.',
-fk_course 			INT UNSIGNED 			NOT NULL 				COMMENT'This field represents the product that was sold.',
+id_sale     INT				AUTO_INCREMENT    COMMENT'This field represents the identifier of the sale.',
+fk_seller   INT UNSIGNED    NOT NULL          COMMENT'This field represents the seller.',
+fk_course   INT UNSIGNED    NOT NULL          COMMENT'This field represents the product that was sold.',
 PRIMARY KEY(id_sale),
 FOREIGN KEY(fk_seller) REFERENCES tbl_Users(id_user),
-FOREIGN KEY(fk_course) REFERENCES tbl_Courses(id_course)) 			COMMENT'This table represents the sales of each user.';
+FOREIGN KEY(fk_course) REFERENCES tbl_Courses(id_course)) COMMENT'This table represents the sales of each user.';
 
 CREATE TABLE IF NOT EXISTS Purchases(
-id_purchase 		INT UNSIGNED 			AUTO_INCREMENT 			COMMENT'This field represents the identifier of the sale.',
-fk_buyer 			INT UNSIGNED 			NOT NULL 				COMMENT'This field represents the buyer.',
-fk_course 			INT UNSIGNED 			NOT NULL 				COMMENT'This field represents the product that was purchased.',
+id_purchase		INT UNSIGNED		AUTO_INCREMENT		COMMENT'This field represents the identifier of the sale.',
+fk_buyer 		INT UNSIGNED 		NOT NULL 			COMMENT'This field represents the buyer.',
+fk_course 		INT UNSIGNED 		NOT NULL 			COMMENT'This field represents the product that was purchased.',
 PRIMARY KEY(id_purchase),
 FOREIGN KEY(fk_buyer) REFERENCES tbl_Users(id_user),
 FOREIGN KEY(fk_course) REFERENCES tbl_Courses(id_course)) COMMENT'This table represents the purchases of each user.';
---
+++ PaginadeInicio/PHP/tables_class.php
@@ -0,0 +1,39 @@
+ <?php
+    class tbl_Categories
+    {
+        private $id_categorie;
+        private $name_categorie;
+        public function __construct($id_categorie, $name_categorie)
+        {
+        }
+    }
+
+    class tbl_Users
+    {
+        private $id_user;
+        private $role_user;
+        private $photo;
+        private $username;
+        private $name_user;
+        private $last_name;
+        private $description_user;
+        private $email;
+        private $password_user;
+        private $registered_date;
+        private $date_of_last_change;
+        public function __construct(
+            $id_user,
+            $role_user,
+            $photo,
+            $username,
+            $name_user,
+            $last_name,
+            $description_user,
+            $email,
+            $password_user,
+            $registered_date,
+            $date_of_last_change
+        ) {
+        }
+    }
+?>
